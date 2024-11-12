cfe855ec920eceb76067e03aef5f67e0477b1376
tmaPeniel <taouemap@gmail.com>
2024-11-12 14:52:29 +0100

    Initialisation du projet

---
+++ GarageASP.NetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GarageASP.NetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
