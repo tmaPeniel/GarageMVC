2a20d6cbd63fa6343108b81a3960cbca70e64b34
tmaPeniel <taouemap@gmail.com>
2024-11-13 17:31:07 +0100

    Modifier voiture

--- GarageASP.NetMVC/Controllers/VoituresController.cs
+++ GarageASP.NetMVC/Controllers/VoituresController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GarageASP.NetMVC.Models;
 using GarageASP.NetMVC.Interfaces;
+using GarageASP.NetMVC.ViewModels;
 
 namespace GarageASP.NetMVC.Controllers
 {
@@ -35,7 +36,42 @@ namespace GarageASP.NetMVC.Controllers
             if (!ModelState.IsValid) return View(voiture);
             _garageManagement.Add(voiture);
             return RedirectToAction("Index");
+        }
 
+        public async Task<IActionResult> EditVoiture(string id)
+        {
+            var voiture = await _garageManagement.GetVoitureByIdAsync(id);
+            if (voiture == null) return View("Error");
+            var voitureVM = new EditVoitureModel
+            {
+                Immatriculation= voiture.Immatriculation,
+                Modele= voiture.Modele,
+                Marque = voiture.Marque,
+                Etat= voiture.Etat
+            };
+            return View(voitureVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditVoiture(string id, EditVoitureModel voitureVM)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Failed to edit club");
+                return View("EditVoiture", voitureVM);
+            };
+
+            var voiture = new Voiture
+            {
+                Immatriculation= id,
+                Modele = voitureVM.Modele,
+                Marque = voitureVM.Marque,
+                Etat = voitureVM.Etat
+            };
+            
+           _garageManagement.Update(voiture);
+            return RedirectToAction("Index");
         }
     }
 }
